79cb4edf2ae61ea15e7d60e5750787de7b0f70fd
zanedr <zanedanielrussell@gmail.com>
2023-01-26 14:48:36 -0700

    add back execute script

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Main from "./Main";
-// import ExecuteAction from "./components/executeAction/ExecuteAction";
+import ExecuteAction from "./components/executeAction/ExecuteAction";
+
 // import PriceCheck from "./components/priceCheck/PriceCheck";
+
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path={'/'} element={<Main/>}/>
-        {/*<Route path={"action"} element={<ExecuteAction/>}/>*/}
+        <Route path={"action"} element={<ExecuteAction/>}/>
         {/*<Route path={"pricing"} element={<PriceCheck/>}/>*/}
       </Routes>
     </div>
