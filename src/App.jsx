fca2b829c7ef21f697baa04e8890754ae66ed840
zanedr <zanedanielrussell@gmail.com>
2023-01-26 14:42:23 -0700

    set app to blank

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import Main from "./Main";
-import ExecuteAction from "./components/executeAction/ExecuteAction";
-import PriceCheck from "./components/priceCheck/PriceCheck";
+// import { Route, Routes } from "react-router-dom";
+// import Main from "./Main";
+// import ExecuteAction from "./components/executeAction/ExecuteAction";
+// import PriceCheck from "./components/priceCheck/PriceCheck";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path={'/'} element={<Main/>}/>
-        <Route path={"action"} element={<ExecuteAction/>}/>
-        <Route path={"pricing"} element={<PriceCheck/>}/>
-      </Routes>
+      AYAYAYAYAYAYAYAY
+      {/*<Routes>*/}
+      {/*  <Route path={'/'} element={<Main/>}/>*/}
+      {/*  <Route path={"action"} element={<ExecuteAction/>}/>*/}
+      {/*  <Route path={"pricing"} element={<PriceCheck/>}/>*/}
+      {/*</Routes>*/}
     </div>
   )
 }
